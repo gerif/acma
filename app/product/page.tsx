74b68905b815b7c4c42c8b33f3467da8e6807549
Erik <erdauletbayangali@gmail.com>
2024-08-13 18:19:22 +0600

    product page is updated

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,14 +1,16 @@
 import clsx from "clsx";
-import {greyQo} from "@/ui/fonts";
+import { greyQo } from "@/ui/fonts";
 
 const Product = () => {
-    const isTrue = 'port';
-    return (
-        <div>
-            <h1 className={clsx({foo: isTrue, bar: 'coin', out: null})}>Product page</h1>
-            <p className={clsx(greyQo.className, 'text-3xl')}>Lorem ipsum</p>
-        </div>
-    );
+  const isTrue = true;
+  return (
+    <div>
+      <h1 className={clsx({ foo: isTrue, bar: "coin", out: null })}>
+        Product page
+      </h1>
+      <p className={clsx(greyQo.className, "text-3xl")}>Lorem ipsum</p>
+    </div>
+  );
 };
 
 export default Product;
